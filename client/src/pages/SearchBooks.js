e4dcb4e045d2fd9f7b1fc8ee876f4a3ca6a03a64
masonMO25 <masonneale@gmail.com>
2023-03-21 14:12:08 -0500

    spelling error

--- client/src/pages/SearchBooks.js
+++ client/src/pages/SearchBooks.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { Jumbotron, Container, Col, Form, Button, Card, CardColumns } from 'react-bootstrap';
 import {useMutation} from '@apollo/client';
-import {SAVE_BOOK} from '../utils/mutations';
+import {SAVE_BOOK} from '../utils/mutaitions';
 import Auth from '../utils/auth';
 import { searchGoogleBooks } from '../utils/API';
 import { getSavedBookIds } from '../utils/localStorage';
